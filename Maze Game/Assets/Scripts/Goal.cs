9c2eb21c320649dc20d7fa98e1b0ced27b3e827c
Nilanga Jayawickrama <jayawickrama.np@gmail.com>
2026-02-25 13:52:45 -0800

    Added changes

--- Maze Game/Assets/Scripts/Goal.cs
+++ Maze Game/Assets/Scripts/Goal.cs
@@ -6,7 +6,7 @@ public class Goal : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 8)
-            SceneManager.LoadScene("main");
+            SceneManager.LoadScene("Scene 02");
     }
 
 
