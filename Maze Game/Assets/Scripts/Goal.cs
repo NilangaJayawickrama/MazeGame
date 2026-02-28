d3fc2ada0d612608ce51723cc8741770c933c491
Nilanga Jayawickrama <jayawickrama.np@gmail.com>
2026-02-28 11:45:08 -0800

    Finished level 01 hazards

--- Maze Game/Assets/Scripts/Goal.cs
+++ Maze Game/Assets/Scripts/Goal.cs
@@ -6,19 +6,6 @@ public class Goal : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 8)
-            SceneManager.LoadScene("Scene 02");
-    }
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+            SceneManager.LoadScene("Main");
     }
 }
