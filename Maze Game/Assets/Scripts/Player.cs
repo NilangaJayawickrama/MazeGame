6cdcd4109a638c8a07f7f61a11d2f26cd55ddbe8
Nilanga Jayawickrama <jayawickrama.np@gmail.com>
2026-02-27 21:28:29 -0800

    Added player dashing

--- Maze Game/Assets/Scripts/Player.cs
+++ Maze Game/Assets/Scripts/Player.cs
@@ -30,75 +30,110 @@ public class Player : MonoBehaviour
 
     private Vector3 movementVelocity = Vector3.zero;
 
+    //Dashing
+    [Header("Dashing")]
+    [Tooltip("Total distance traveled during a dash.")]
+    public float dashDistance = 17;
+    [Tooltip("Time taken for a dash (in seconds).")]
+    public float dashTime = .26f;
+
+    private bool IsDashing
+    {
+        get
+        {
+            return (Time.time < dashBeginTime + dashTime);
+        }
+    }
+
+    private Vector3 dashDirection;
+    private float dashBeginTime = Mathf.NegativeInfinity;
+
+    [Tooltip("Time after dashing finishes before it can be performed again.")] //to reduce overlapping two dashes
+    public float dashCooldown = 1.8f;
+
+    private bool CanDashNow
+    {
+        get
+        {
+            return (Time.time > dashBeginTime + dashTime + dashCooldown);
+        }
+    }
+
+
+
     // Method to handle movement logic
     private void Movement()
     {
-        // Forward Movement (Z Axis)
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if (!IsDashing)
         {
-            if (movementVelocity.z >= 0)
-                movementVelocity.z = Mathf.Min(movespeed, movementVelocity.z + VelocityGainPerSecond * Time.deltaTime);
+            // Forward Movement (Z Axis)
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                if (movementVelocity.z >= 0)
+                    movementVelocity.z = Mathf.Min(movespeed, movementVelocity.z + VelocityGainPerSecond * Time.deltaTime);
+                else
+                    movementVelocity.z = Mathf.Min(0, movementVelocity.z + VelocityGainPerSecond * reverseMomentumMultiplier * Time.deltaTime);
+            }
+
+            // Backward Movement (Z Axis)
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                if (movementVelocity.z > 0)
+                    movementVelocity.z = Mathf.Max(0, movementVelocity.z - VelocityGainPerSecond * reverseMomentumMultiplier * Time.deltaTime);
+                else
+                    movementVelocity.z = Mathf.Max(-movespeed, movementVelocity.z - VelocityGainPerSecond * Time.deltaTime);
+            }
+
+            // If neither forward nor back are being held
             else
-                movementVelocity.z = Mathf.Min(0, movementVelocity.z + VelocityGainPerSecond * reverseMomentumMultiplier * Time.deltaTime);
-        }
+            {
+                if (movementVelocity.z > 0)
+                    movementVelocity.z = Mathf.Max(0, movementVelocity.z - VelocityLossPerSecond * Time.deltaTime);
+                else
+                    movementVelocity.z = Mathf.Min(0, movementVelocity.z + VelocityLossPerSecond * Time.deltaTime);
+            }
 
-        // Backward Movement (Z Axis)
-        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-        {
-            if (movementVelocity.z > 0)
-                movementVelocity.z = Mathf.Max(0, movementVelocity.z - VelocityGainPerSecond * reverseMomentumMultiplier * Time.deltaTime);
+            // Right Movement (X Axis)
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                if (movementVelocity.x >= 0)
+                    movementVelocity.x = Mathf.Min(movespeed, movementVelocity.x + VelocityGainPerSecond * Time.deltaTime);
+                else
+                    movementVelocity.x = Mathf.Min(0, movementVelocity.x + VelocityGainPerSecond * reverseMomentumMultiplier * Time.deltaTime);
+            }
+
+            // Left Movement (X Axis)
+            else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                if (movementVelocity.x > 0)
+                    movementVelocity.x = Mathf.Max(0, movementVelocity.x - VelocityGainPerSecond * reverseMomentumMultiplier * Time.deltaTime);
+                else
+                    movementVelocity.x = Mathf.Max(-movespeed, movementVelocity.x - VelocityGainPerSecond * Time.deltaTime);
+            }
+
+            // If neither right nor left are being held
             else
-                movementVelocity.z = Mathf.Max(-movespeed, movementVelocity.z - VelocityGainPerSecond * Time.deltaTime);
-        }
+            {
+                if (movementVelocity.x > 0)
+                    movementVelocity.x = Mathf.Max(0, movementVelocity.x - VelocityLossPerSecond * Time.deltaTime);
+                else
+                    movementVelocity.x = Mathf.Min(0, movementVelocity.x + VelocityLossPerSecond * Time.deltaTime);
+            }
 
-        // If neither forward nor back are being held
-        else
-        {
-            if (movementVelocity.z > 0)
-                movementVelocity.z = Mathf.Max(0, movementVelocity.z - VelocityLossPerSecond * Time.deltaTime);
-            else
-                movementVelocity.z = Mathf.Min(0, movementVelocity.z + VelocityLossPerSecond * Time.deltaTime);
-        }
+            if (movementVelocity.x != 0 || movementVelocity.z != 0)
+            {
+                characterController.Move(movementVelocity * Time.deltaTime);
+                modelTrans.rotation = Quaternion.Slerp(modelTrans.rotation, Quaternion.LookRotation(movementVelocity), .18F);
+            }
 
-        // Right Movement (X Axis)
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            if (movementVelocity.x >= 0)
-                movementVelocity.x = Mathf.Min(movespeed, movementVelocity.x + VelocityGainPerSecond * Time.deltaTime);
-            else
-                movementVelocity.x = Mathf.Min(0, movementVelocity.x + VelocityGainPerSecond * reverseMomentumMultiplier * Time.deltaTime);
         }
-
-        // Left Movement (X Axis)
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        {
-            if (movementVelocity.x > 0)
-                movementVelocity.x = Mathf.Max(0, movementVelocity.x - VelocityGainPerSecond * reverseMomentumMultiplier * Time.deltaTime);
-            else
-                movementVelocity.x = Mathf.Max(-movespeed, movementVelocity.x - VelocityGainPerSecond * Time.deltaTime);
-        }
-
-        // If neither right nor left are being held
-        else
-        {
-            if (movementVelocity.x > 0)
-                movementVelocity.x = Mathf.Max(0, movementVelocity.x - VelocityLossPerSecond * Time.deltaTime);
-            else
-                movementVelocity.x = Mathf.Min(0, movementVelocity.x + VelocityLossPerSecond * Time.deltaTime);
-        }
-
-        if (movementVelocity.x != 0 || movementVelocity.z != 0)
-        {
-            characterController.Move(movementVelocity * Time.deltaTime);
-            modelTrans.rotation = Quaternion.Slerp(modelTrans.rotation, Quaternion.LookRotation(movementVelocity), .18F);
-        }
-
     }
 
     // Call the Movement method in the Update method
     private void Update()
     {
         Movement();
+        Dashing();
     }
 
     // Death and Respawning Variables
@@ -113,6 +148,39 @@ public class Player : MonoBehaviour
         spawnPoint = transform.position;
     }
 
+    private void Dashing()
+    {
+        if (!IsDashing && CanDashNow) //If not dashing right now
+        {
+            if (Input.GetKey(KeyCode.Space)) //If space key is pressed
+            {
+                Vector3 movementDir = Vector3.zero;
+
+                if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                    movementDir.z = 1;
+                else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                    movementDir.z = -1;
+
+                if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                    movementDir.x = 1;
+                else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                    movementDir.x = -1;
+
+                if (movementDir.x != 0 || movementDir.z != 0)
+                {
+                    dashDirection = movementDir;
+                    dashBeginTime = Time.time;
+                    movementVelocity = dashDirection * movespeed;
+                    modelTrans.forward = dashDirection;
+                }
+            }
+        }
+        else //If dashing
+        {
+            characterController.Move(dashDirection * (dashDistance / dashTime) * Time.deltaTime);
+        }
+    }
+
     public void Die()
     {
         if (!dead)
@@ -123,6 +191,7 @@ public class Player : MonoBehaviour
             enabled = false;
             characterController.enabled = false;
             modelTrans.gameObject.SetActive(false);
+            dashBeginTime = Mathf.NegativeInfinity;
         }
     }
 
